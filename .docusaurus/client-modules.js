7f790a188ca2f7d694a25ef7667b9ac54086e684
John Vastola <jvastola@ucmerced.edu>
2025-01-02 14:31:08 -0800

    Deploy website - based on 2ed075c93eafeb9afee836b0854ef33e90bfa351

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/johnnyvastola/Desktop/horizonworlds/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/johnnyvastola/Desktop/horizonworlds/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/johnnyvastola/Desktop/horizonworlds/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/johnnyvastola/Desktop/horizonworlds/src/css/custom.css"),
-];
